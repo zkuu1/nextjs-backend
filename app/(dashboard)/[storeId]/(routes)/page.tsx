9841f75ee7638d5fb0410d819a02a6f67520af8b
Zkuu <zakariasemarang33@gmail.com>
2025-01-08 22:57:31 +0700

    change bg

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,21 +1,27 @@
 import db from "@/lib/db";
 
 interface DashboardPageProps {
-    params: {storeId: string}
+  params: { storeId: string };
 }
 
-const DashboardPage = async ({params} : DashboardPageProps) => {
-   const store = await db.store.findFirst({
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const store = await db.store.findFirst({
     where: {
-        id: params.storeId
-    }
-   })
+      id: params.storeId,
+    },
+  });
 
-   return (
-    <div>
-        Active Store = {store?.name}
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <div className="bg-white rounded-lg shadow-lg p-8 text-center w-3/4 max-w-md text-orange-700">
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <p className="text-lg">
+          <span className="font-semibold">Kamu Berada Di Catering Yaitu</span>{" "}
+          {store?.name || "No Store Found"}
+        </p>
+      </div>
     </div>
-   )
-}
+  );
+};
 
 export default DashboardPage;
